83a1b0daff0303cde3e0479fcbd51ab5c8a93853
emon02 <abdulalimemon11@gmail.com>
2025-02-23 22:34:29 +0600

    setup eslint

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,19 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express, { Application, Request, Response } from "express";
+import cors from "cors";
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app: Application = express();
+const port = 3000;
+
+// parsers
+app.use(express.json());
+app.use(cors);
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
+export default app;
